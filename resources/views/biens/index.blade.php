758acda6e44096ddf472df5d9d31abab1ca46038
Adama Dabo <dabodabo9064@gmail.com>
2024-06-06 18:53:29 +0000

    reglage des conflit et aussi faire des brains pour améliorer nos pages

--- resources/views/biens/index.blade.php
+++ resources/views/biens/index.blade.php
@@ -1,28 +1,24 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Accueil - Gestion Immobilière</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">  
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Accueil - Gestion Immobilière</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <style>
     .navbar-custom {
-      height: 90px; /* Hauteur normale pour la barre de navigation */
+      height: 90px;
     }
     .navbar-custom .nav-link {
-      font-size: 1.25rem; /* Taille moyenne pour les écritures */
+      font-size: 1.25rem;
     }
     .navbar-custom .navbar-brand {
-      font-size: 1.6rem; /* Taille un peu plus grande pour le logo/marque */
+      font-size: 1.6rem;
     }
     .btn-primary {
-      font-size: 1rem; /* Taille moyenne pour le bouton */
+      font-size: 1rem;
     }
-
     .about-section {
       padding: 60px 0;
       background-color: #f8f9fa;
@@ -39,7 +35,7 @@
       display: block;
       width: 60px;
       height: 4px;
-      background-color:#0056b3 ;
+      background-color: #0056b3;
       margin: 20px auto;
       border-radius: 2px;
     }
@@ -53,7 +49,6 @@
       border-radius: 10px;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
-    /* Animations */
     .fade-in {
       animation: fadeIn 2s ease-in-out;
     }
@@ -81,106 +76,81 @@
     }
     footer .social-icons a:hover {
       color: #ff6137;
-    }   
-
+    }
   </style>
-  <body>
+</head>
+<body>
 
-    {{-- section barre de navigation --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
-        <div class="container">
-
-          <a class="navbar-brand" href="#">Damask Immobilier</a>
-          <div class="collapse navbar-collapse justify-content-center CENTRE" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/"><strong>Accueil</strong></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><strong>A Propos</strong></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><strong>Contact</strong></a>
-              </li>
-            </ul>
-          </div>
-          <div class="navbar-nav ml-auto">
-            <a href="login"><button class="btn btn-primary" type="button">Se connecter</button></a> 
-          </div>
-
-
-            <a class="navbar-brand" href="#">Damask Immobilier
-                {{-- <img src="logo.png" alt="Damask Immobilier" style="height: 40px;"> --}}
-            </a>
-            
- 
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/"><strong>Accueil</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><strong>A Propos</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact"><strong>Contact</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true"></a>
-                    </li>
-                </ul>
-            </div>
-            
-    
-            <div class="navbar-nav ml-auto">
-               <a href="login"><button class="btn btn-primary" type="button">Se connecter</button></a> 
-            </div>
-
-        </div>
-      </nav>
-
-    {{-- section banniere --}}
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="card text-bg-dark">
-                    <img src="https://images.pexels.com/photos/1571470/pexels-photo-1571470.jpeg?auto=compress&cs=tinysrgb&w=600" class="d-block w-100" style="height: 80vh" alt="...">
-                    <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
-                        <h2 class="card-title" style="color: white;">BIENVENUE</h2>
-                        <p class="card-text" style="color: white;"> <h3>Sur DAMASK immobilier.</h3></p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="card text-bg-dark">
-                    <img src="https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  style="height: 80vh" class="d-block w-100" alt="...">
-                    <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
-                        <h2 class="card-title" style="color: white;">BIENVENUE</h2>
-                        <p class="card-text" style="color: white;"> <h3>Sur DAMASK immobilier.</h3></p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="card text-bg-dark">
-                    <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600" style="height: 80vh" class="d-block w-100" alt="...">
-                    <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
-                        <h2 class="card-title" style="color: white;">BIENVENUE</h2>
-                        <p class="card-text" style="color: black;"> <h3>Sur DAMASK immobilier.</h3></p>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+  {{-- section barre de navigation --}}
+  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+    <div class="container">
+      <a class="navbar-brand" href="#">Damask Immobilier</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/"><strong>Accueil</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>A Propos</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact"><strong>Contact</strong></a>
+          </li>
+        </ul>
+      </div>
+      <a href="login" class="btn btn-primary" type="button">Se connecter</a>
     </div>
+  </nav>
 
+  {{-- section banniere --}}
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="card text-bg-dark">
+          <img src="https://images.pexels.com/photos/1571470/pexels-photo-1571470.jpeg?auto=compress&cs=tinysrgb&w=600" class="d-block w-100" style="height: 80vh" alt="...">
+          <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
+            <h2 class="card-title" style="color: white;">BIENVENUE</h2>
+            <p class="card-text" style="color: white;">
+              <h3>Sur DAMASK immobilier.</h3>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="card text-bg-dark">
+          <img src="https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style="height: 80vh" class="d-block w-100" alt="...">
+          <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
+            <h2 class="card-title" style="color: white;">BIENVENUE</h2>
+            <p class="card-text" style="color: white;">
+              <h3>Sur DAMASK immobilier.</h3>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="card text-bg-dark">
+          <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600" style="height: 80vh" class="d-block w-100" alt="...">
+          <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
+            <h2 class="card-title" style="color: white;">BIENVENUE</h2>
+            <p class="card-text" style="color: black;">
+              <h3>Sur DAMASK immobilier.</h3>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
   <!-- Section "À propos" -->
   <section class="about-section">
@@ -189,17 +159,17 @@
       <div class="row align-items-center">
         <div class="col-md-6 fade-in">
           <p>
-            Damask Immobilier est une entreprise leader dans le secteur de l'immobilier. 
-            Nous nous engageons à offrir à nos clients un service de qualité supérieure, 
-            des conseils d'experts et des solutions innovantes pour toutes leurs 
-            transactions immobilières. Que vous cherchiez à acheter, vendre ou louer 
+            Damask Immobilier est une entreprise leader dans le secteur de l'immobilier.
+            Nous nous engageons à offrir à nos clients un service de qualité supérieure,
+            des conseils d'experts et des solutions innovantes pour toutes leurs
+            transactions immobilières. Que vous cherchiez à acheter, vendre ou louer
             une propriété, notre équipe dédiée est là pour vous accompagner à chaque étape.
           </p>
           <p>
-            Nous croyons en la transparence, l'intégrité et la satisfaction de nos clients. 
-            Notre mission est de rendre le processus immobilier aussi simple et agréable que 
-            possible pour vous. Avec des années d'expérience et une connaissance approfondie 
-            du marché, Damask Immobilier est votre partenaire de confiance pour toutes vos 
+            Nous croyons en la transparence, l'intégrité et la satisfaction de nos clients.
+            Notre mission est de rendre le processus immobilier aussi simple et agréable que
+            possible pour vous. Avec des années d'expérience et une connaissance approfondie
+            du marché, Damask Immobilier est votre partenaire de confiance pour toutes vos
             transactions immobilières.
           </p>
         </div>
@@ -210,15 +180,15 @@
     </div>
   </section>
 
-    <div class="container mt-5">
-        <h2 style="text-align: center; font-size: 2.5rem; color: #343a40;">Nos Biens Immobiliers</h2>
-        <div class="row">
-            @foreach ($biens as $bien)
-            <div class="col-md-4 mb-4">
-                <div class="card h-100 shadow-sm">
-                    <img src="{{$bien->image}}" class="card-img-top" alt="Image du bien">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">{{$bien->nom}}</h5>
+  <div class="container mt-5">
+    <h2 style="text-align: center; font-size: 2.5rem; color: #343a40;">Nos Biens Immobiliers</h2>
+    <div class="row">
+      @foreach ($biens as $bien)
+      <div class="col-md-4 mb-4">
+        <div class="card h-100 shadow-sm">
+          <img src="{{$bien->image}}" class="card-img-top" alt="Image du bien">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">{{$bien->nom}}</h5>
                         <p class="card-text flex-grow-1">
                             <strong>Catégorie :</strong> {{$bien->categorie}}<br>
                             <strong>Description :</strong> {{ Str::limit($bien->description , 100);}}<br>
